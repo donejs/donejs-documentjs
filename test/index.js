cfdf2982b4f78b21eefbec046edfda9cad2f9af7
Kevin Phillips <kphillips86@gmail.com>
2017-02-01 18:30:54 -0600

    reading name and folder from package.json if available

--- test/index.js
+++ test/index.js
@@ -1,41 +1,83 @@
 var path = require('path');
 var helpers = require('yeoman-test');
+var fs = require('fs-extra');
 var assert = require('yeoman-assert');
 
 describe('donejs-documentjs', function() {
-  before(function(done) {
-    helpers.run(path.join(__dirname, '../default'))
-      .inTmpDir()
-      .withPrompts({
-				name: 'my-app',
-				folder: 'src'
-      }).on('end', done);
-  });
+	describe('when there is no package.json', function() {
+		before(function(done) {
+			helpers.run(path.join(__dirname, '../default'))
+				.inTmpDir()
+				.withPrompts({
+					name: 'my-app',
+					folder: 'src'
+				}).on('end', done);
+		});
 
-  it('should add documentjs script to package.json', function() {
-    assert.file(['package.json']);
-    assert.JSONFileContent('package.json', {
-      scripts: {
-        document: "documentjs"
-      }
-    });
-  });
+		it('should add documentjs script to package.json', function() {
+			assert.file(['package.json']);
+			assert.JSONFileContent('package.json', {
+				scripts: {
+					document: "documentjs"
+				}
+			});
+		});
 
-  it('should write documentjs.json file', function() {
-    assert.file(['documentjs.json']);
-    assert.JSONFileContent('documentjs.json', {
-			"sites": {
-				"docs": {
-					"dest": "docs",
-					"parent": "my-app",
-					"pageConfig": {
-						"page": "docs"
-					},
-					"glob": {
-						"pattern": "src/**/*.{js,md}"
+		it('should write documentjs.json file', function() {
+			assert.file(['documentjs.json']);
+			assert.JSONFileContent('documentjs.json', {
+				"sites": {
+					"docs": {
+						"dest": "docs",
+						"parent": "my-app",
+						"pageConfig": {
+							"page": "docs"
+						},
+						"glob": {
+							"pattern": "src/**/*.{js,md}"
+						}
 					}
 				}
-			}
+			});
+		});
+	});
+
+	describe('when there is a package.json with name and directories.lib', function() {
+		before(function(done) {
+			helpers.run(path.join(__dirname, '../default'))
+				.inTmpDir(function(tmpDir) {
+					var src = path.join(__dirname, 'files/package.json');
+					var dest = path.join(tmpDir, 'package.json');
+					fs.copySync(src, dest);
+				})
+				.on('end', done);
+		});
+
+		it('should add documentjs script to package.json', function() {
+			assert.file(['package.json']);
+			assert.JSONFileContent('package.json', {
+				scripts: {
+					document: "documentjs"
+				}
+			});
+		});
+
+		it('should write documentjs.json file', function() {
+			assert.file(['documentjs.json']);
+			assert.JSONFileContent('documentjs.json', {
+				"sites": {
+					"docs": {
+						"dest": "docs",
+						"parent": "package-json-app",
+						"pageConfig": {
+							"page": "docs"
+						},
+						"glob": {
+							"pattern": "directories-lib/**/*.{js,md}"
+						}
+					}
+				}
+			});
 		});
 	});
 });
